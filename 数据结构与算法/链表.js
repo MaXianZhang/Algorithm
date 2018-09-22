49847ed222f1aca5465a4db43e17040ab8b2be42
jason <122230606@qq.com>
2018-09-22 22:12:26 +0800

    lianbiao2

--- 数据结构与算法/链表.js
+++ 数据结构与算法/链表.js
@@ -3,9 +3,21 @@ function Node(element) {
   this.next = null
 }
 
+function DoublyNode() {
+  this.element = element
+  this.next = null
+  this.prev = null
+}
+
 function LinkedList() {
   this.length = 0
   this.head = null
+}
+
+function DoublyLinkedList() {
+  this.length = 0
+  this.head = null
+  this.tail = null
 }
 
 LinkedList.prototype = {
@@ -49,6 +61,63 @@ LinkedList.prototype = {
     }
   },
 
+  insert: function(position, element) {
+    if(position >= 0 && position <= length) {
+      var node = new Node(element)
+      var current = head
+      var previous
+      var index = 0
+
+      if (position === 0) {
+        node.next = current
+        head = node
+      } else {
+        while (index++ < position) {
+          previous = current
+          current = current.next
+        }
+        node.next = current
+        previous.next = node
+      }
+      length++
+
+      return true
+    } else {
+      return false
+    }
+  },
+
+   toString: function() {
+     var current = this.head
+     var string = ''
+
+     while (current) {
+       string = current.element
+       current = current.next
+     }
+
+     return string
+   },
+
+   index: function (element) {
+    var current = this.head
+    var index = -1
+
+    while(current) {
+      if(element === current.element) {
+        return index
+      }
+      index++
+      current = current.next
+    }
+
+    return -1
+   },
+
+   remove: function(element) {
+     var index = this.indexOf(element)
+     return this.removeAt(index)
+   },
 
 }
 
