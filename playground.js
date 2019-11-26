5c2088cd6c4f86ff0c004dd251b641d8a7577377
jason <122230606@qq.com>
2019-11-26 19:02:17 +0800

    thinking

--- playground.js
+++ playground.js
@@ -4,19 +4,44 @@ matrix = [
   [1, 0, 1, 0, 0],
   [1, 0, 1, 1, 1],
   [1, 1, 1, 1, 1],
-  [1, 0, 0, 1, 0],
+  [1, 0, 1, 1, 0],
 ]
 
 var maximalSquare = function (matrix) {
-  let dp = []
-  const lenX = matrix[0].length
-  const lenY = matrix.length
+  var dp = []
+  var lenX = matrix[0].length
+  var lenY = matrix.length
 
-  for (let i = 0; i < lenX; i++) {
-    for (let j = 0; j < lenY; j++) {
-      
+
+  for (var i = 0; i < lenY; i++) {
+    dp[i] = []
+    dp[i][0] = matrix[i][0]
+  }
+  dp[0] = matrix[0]
+
+
+
+  for (var i = 1; i < lenY; i++) {
+    for (var j = 1; j < lenX; j++) {
+      var top = dp[i - 1][j]
+      var left = dp[i][j - 1]
+      var topleft = dp[i - 1][j - 1]
+      var cur = matrix[i][j]
+      if(cur === 1) {
+        if(top === left && left === topleft && topleft === top) {
+          dp[i][j] = top + 1
+        } else {
+          dp[i][j] = 1
+        }
+      } else {
+        dp[i][j] = 0
+      }
     }
   }
+  log(dp)
+
+
+  
 
 };
 
