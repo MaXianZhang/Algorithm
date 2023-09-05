39a839ac030b2cad4d291a30f274e2f8f64320e1
MaXianZhang <zhangxu6271@163.com>
2023-09-05 23:58:07 +0800

    heap

--- utils/heap.js
+++ utils/heap.js
@@ -1,51 +1,75 @@
 class Heap {
-    constructor() {
-
+    constructor(type = 'max') {
+        this.type = type;
+        this.heap = [];
     }
 
-    peek() {
-
+    // 将新元素添加到堆中  
+    push(value) {
+        this.heap.push(value);
+        this.heapifyUp(this.heap.length - 1);
     }
 
+    // 从堆中移除元素  
     pop() {
-
+        if (this.heap.length === 0) {
+            return null; // 或者抛出错误  
+        }
+        const value = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.heapifyDown(0);
+        return value;
     }
 
-    push() {
+    // 获取堆顶元素，但不移除它  
+    peek() {
+        return this.heap[0];
+    }
 
+    // 将数组转换为堆  
+    fromArray(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            this.push(arr[i]);
+        }
+    }
+
+    // 转换为数组  
+    toArray() {
+        return this.heap;
+    }
+
+    // 堆化数组（上）  
+    heapifyUp(index) {
+        const parentIndex = Math.floor((index - 1) / 2);
+        if (index <= 0 || this.heap[parentIndex] >= this.heap[index]) {
+            return; // 父节点大于或等于子节点，无需调整  
+        }
+        if (this.type === 'min') {
+            [this.heap[parentIndex], this.heap[index]] = [this.heap[index], this.heap[parentIndex]]; // 交换位置  
+        } else {
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]]; // 交换位置  
+        }
+        this.heapifyUp(parentIndex); // 递归调整父节点  
+    }
+
+    // 堆化数组（下）  
+    heapifyDown(index) {
+        const leftChildIndex = index * 2 + 1; // 左子节点索引  
+        const rightChildIndex = index * 2 + 2; // 右子节点索引  
+        let smallestIndex = index; // 最小的子节点索引  
+        if (leftChildIndex < this.heap.length && this.heap[leftChildIndex] < this.heap[smallestIndex]) {
+            smallestIndex = leftChildIndex; // 更新最小子节点索引为左子节点索引  
+        }
+        if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] < this.heap[smallestIndex]) {
+            smallestIndex = rightChildIndex; // 更新最小子节点索引为右子节点索引  
+        }
+        if (smallestIndex !== index) { // 如果最小的子节点不是当前节点，交换它们的位置并递归调整子节点  
+            if (this.type === 'min') {
+                [this.heap[index], this.heap[smallestIndex]] = [this.heap[smallestIndex], this.heap[index]]; // 交换位置  
+            } else {
+                [this.heap[smallestIndex], this.heap[index]] = [this.heap[index], this.heap[smallestIndex]]; // 交换位置  
+            }
+            this.heapifyDown(smallestIndex); // 递归调整最小的子节点  
+        }
     }
 }
-
-// TODO: 实现最大堆和最小堆
-
-
-// 任何要求我们在给定集合中找到最高 / 最小 / 频繁的“ k”元素的问题都属于这种模式。
-// 跟踪“ k”元素的最佳数据结构是 Heap。 这个模式将利用堆来解决多个问题，一次处理一组给定元素中的“ k”元素。
-
-
-// T给定一个未排序的数字数组，找出其中最大的“ k”数。
-
-// const Heap = require('collections/heap');
-
-// function find_k_largest_numbers(nums, k) {
-//   const minHeap = new Heap([], null, ((a, b) => b - a));
-//   // 存进k个值，要求是最小堆，这样依赖堆定就是k个元素中最小的
-//   for (i = 0; i < k; i++) {
-//     minHeap.push(nums[i]);
-//   }
-
-//   // 谁比堆顶大，谁就进堆，堆顶出去，慢走不送
-//   for (i = k; i < nums.length; i++) {
-//     if (nums[i] > minHeap.peek()) {
-//       minHeap.pop();
-//       minHeap.push(nums[i]);
-//     }
-//   }
-
-//   // the heap has the top 'K' numbers, return them in a list
-//   return minHeap.toArray();
-// }
-
-
-// console.log(`Here are the top K numbers: ${find_k_largest_numbers([3, 1, 5, 12, 2, 11], 3)}`);
-// console.log(`Here are the top K numbers: ${find_k_largest_numbers([5, 12, 11, -1, 12], 3)}`);
